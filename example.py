385cbd5c3926ab21d6dfe76129bf90d798521c1d
skythaddeus <markmanuel@skydev.solutions>
2024-11-14 12:31:59 +0800

    Added dot counter

--- example.py
+++ example.py
@@ -1,7 +1,9 @@
 import cv2
 import pickle
+import numpy as np
+import cvzone
 
-width, height = 320, 400
+width, height = 800, 1000
 
 move_offset = 400
 
@@ -10,6 +12,25 @@ try:
         posList = pickle.load(f)
 except:
     posList = []
+
+def checkParkingSpace(imgPro):
+    for pos in posList:
+        x,y = pos
+        imgCrop = imgPro[y:y+height, x:x+width]
+        cv2.imshow(str(x+y), imgCrop)
+        count = cv2.countNonZero(imgCrop)
+
+        cvzone.putTextRect(img, str(count), (x, y+height - 10), scale=1.5, thickness=2, offset=0)
+
+        if count > 800:
+            color = (0,255,0)
+            thickness = 5
+        else:
+            color = (0,0,255)
+            thickness = 2
+
+        cv2.rectangle(img, pos, (pos[0]+width, pos[1] + height), color, thickness=thickness)
+
 
 
 def mouseClick(events,x,y,flags,params):
@@ -27,12 +48,27 @@ def mouseClick(events,x,y,flags,params):
         pickle.dump(posList, f)
 
 while True:
-    img = cv2.imread("ExampleParking.png")
+    img = cv2.imread("Parking2.jpg")
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (3,3),1)
+    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 25,25)
+    imgMedian = cv2.medianBlur(imgThreshold, 5)
+
+
+    kernel = np.ones((3,3), np.uint8)
+    imgDilate = cv2.dilate(imgMedian, kernel, iterations=1)
+
+
 
     # cv2.rectangle(img,(200, 100), (550 ,500), (255,0,255), 2)
-    for pos in posList:
-        cv2.rectangle(img, pos, (pos[0]+width, pos[1] + height), (255,0,255), 2)
+    # for pos in posList:
+    #     cv2.rectangle(img, pos, (pos[0]+width, pos[1] + height), (255,0,255), 2)
+
+    checkParkingSpace(imgDilate)
 
     cv2.imshow("Window", img)
+    # cv2.imshow("Dilated", imgDilate)
+
     cv2.setMouseCallback("Window", mouseClick)
     cv2.waitKey(1)
